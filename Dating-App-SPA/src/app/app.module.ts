123698d9f11befb0c6c9ed19bc2495576234926b
Student255 <daniel.leczycki@student.wat.edu.pl>
2019-09-16 11:30:27 +0100

    End of Section 10

--- Dating-App-SPA/src/app/app.module.ts
+++ Dating-App-SPA/src/app/app.module.ts
@@ -26,6 +26,9 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -48,7 +51,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -68,9 +72,11 @@ export class CustomHammerConfig extends HammerGestureConfig {
   ],
   providers: [
     AuthService,
+    PreventUnsavedChanges,
     ErrorInterceptorProvider,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
   ],
   bootstrap: [AppComponent]
